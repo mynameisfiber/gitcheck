1cf34127bc71c6336c4957a899c180941956b2ad
Micha Gorelick <mynameisfiber@gmail.com>
2011-06-24 13:53:01 -0400

    First Commit... seems to be working fine, still debugging though.

---
+++ Repository.py
@@ -0,0 +1,94 @@
+#!/bin/env python
+import os
+from hashlib import md5
+
+class Repository:
+  def __init__(self, location, GIT_PATH="/usr/bin"):
+    self.GIT_PATH = GIT_PATH
+    self.location = location
+    self.name     = os.path.basename(self.location)
+
+    self.update_repo()   
+    self.remotes      = self.find_remotes()
+    self.local_heads  = self.find_local_heads()
+
+    self.updates = {}
+    self.updates_keys = []
+    self.check_updates()
+
+  def has_updates(self):
+    for key, data in self.updates.iteritems():
+      if data["new"]:
+        return True
+    return False
+
+  def get_new_updates(self):
+    for key, data in self.updates.iteritems():
+      if data["new"]:
+        yield (key, data)
+        data["new"] = False
+
+  def check_updates(self):
+    updates = {}
+    hasupdate = False
+    for remote,refs in self.remotes.iteritems():
+      for head, checksum in refs.iteritems():
+        try:
+          if self.local_heads[head] != checksum:
+            update = {}
+            update["repo"] = self.name
+            update["ref"] = "%s/%s"%(remote,head)
+            update["desc"]  = self.get_commit_desc(remote, head)[0]
+            update["desc"] = update["desc"][len(update["ref"])+3:].strip() #cleanup
+            update["new"] = True
+            key = md5(update["desc"]).hexdigest()
+
+            if key not in self.updates_keys:
+              self.updates_keys.append(key)
+              self.updates.update( {md5(update["desc"]).hexdigest() : update} )
+              hasupdate |= True
+        except KeyError:
+          pass
+    return hasupdate
+
+  def _run_git_cmd(self, cmd):
+    pwd = os.getcwd()
+    os.chdir(self.location)
+    pd = os.popen("%s/git %s"%(self.GIT_PATH,cmd))
+    mesg = pd.readlines()
+    retval = pd.close()
+    os.chdir(pwd)
+    return {"retval" : retval, "mesg" : mesg}
+
+  def get_commit_desc(self, remote, head):
+    output = self._run_git_cmd("show-branch %s/%s"%(remote, head))
+    if output["retval"] is None:
+      return output["mesg"]
+    else:
+      return "Error: %s"%output["mesg"]
+
+  def update_repo(self):
+    output = self._run_git_cmd("remote update")
+    if output["retval"] is not None:
+      raise Exception("%s: Problem updating remote"%self.name)
+
+  def find_local_heads(self):
+    heads = {}
+    path = os.path.join(self.location, ".git", "refs", "heads")
+    for head in os.listdir(path):
+      heads.update( {head : file(os.path.join(path,head)).readline().strip()} )
+    return heads
+
+  def find_remotes(self):
+    remote_path = os.path.join(self.location, ".git", "refs", "remotes")
+    remotes = {}
+    try:
+      for remote in os.listdir(remote_path):
+        refs = {}
+        refs_path = os.path.join(remote_path, remote)
+        for ref in os.listdir(refs_path):
+          refs.update( {ref : file(os.path.join(refs_path, ref)).readline().strip()} )
+        remotes.update( {remote : refs} )
+      return remotes
+    except OSError:
+      return {}
---
+++ gitcheck.py
@@ -0,0 +1,54 @@
+#!/bin/env python
+
+import pynotify
+import os
+from Repository import Repository
+from time import sleep
+
+_CONFIG = {"MAXDEPTH"        : 20,
+           "GIT_PATH"        : "/usr/bin",
+           "project_folders" : ['~/projects/',],
+           "icon"            : "git.svg",
+           "check_freq"      : 300}
+
+def find_repo(folder, maxdepth=-1):
+  results = []
+  if maxdepth == 0:
+    return results
+  folder = os.path.expanduser(folder)
+  for item in os.listdir(folder):
+    full_item = os.path.join(folder, item)
+    if os.path.isdir(full_item):
+      if item == ".git":
+        results.append(folder)
+      else:
+        results += find_repo(full_item,maxdepth-1)
+  return results
+
+def show_message(title, message,icon="git.svg"):
+  icon = os.path.join(os.getcwd(),icon)
+  msg = pynotify.Notification(title, 
+                              message,
+                              icon)
+  if not msg.show():
+    print "Could not display message: (%s) %s"%(title, message)
+                        
+
+if __name__ == "__main__":
+  if not pynotify.init ("icon-summary-body"):
+    print "Could not initialize notification system"
+    exit(1)
+
+  repos = []
+  for project_folder in _CONFIG["project_folders"]:
+    raw_repos = find_repo(project_folder, maxdepth=_CONFIG["MAXDEPTH"])
+    for raw_repo in raw_repos:
+      repos.append(Repository(raw_repo,GIT_PATH=_CONFIG["GIT_PATH"]))
+
+  while True:
+    for repo in repos:
+      if repo.has_updates():
+        for key, update in repo.get_new_updates():
+          print update
+          show_message(update["repo"], update["desc"])
+    sleep(_CONFIG["check_freq"])
